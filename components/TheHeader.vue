c9b92e00154e8f332872e132cdd27a0f609b06d2
newbpydev <newbpydev@gmail.com>
2022-09-07 15:47:08 -0300

    initial commit

---
+++ components/TheHeader.vue
@@ -0,0 +1,13 @@
+<template>
+  <header class="header">
+    SomeText
+  </header>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <TheHeader />
+    <main>
+      <slot />
+    </main>
+  </div>
+</template>
+
+<script setup lang="ts">
+import "@/assets/css/style.css";
+
+useHead({
+  title: "Natours | Exciting tours for adventurous people",
+  charset: "utf-8",
+  viewport: "width=device-width, initial-scale=1.0",
+  link: [
+    // { rel: "stylesheet", href: "css/icon-font.css" },
+    // { rel: "stylesheet", href: "css/style.css" },
+    {
+      href: "https://fonts.googleapis.com/css?family=Lato:100,300,400,700,900",
+      rel: "stylesheet",
+    },
+  ],
+});
+</script>
+
+<style scoped>
+/*
+COLORS:
+
+Light green: #7ed56f
+Medium green: #55c57a
+Dark green: #28b485
+
+*/
+</style>

9b18588bdf03c41d322736015dda545de9544734
newbpydev <newbpydev@gmail.com>
2022-09-13 09:25:30 -0300

    style: header, p, and button styles

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,36 @@
 <template>
+  <section class="section-about">
+    <div class="u-center-text u-margin-bottom-big">
+      <h2 class="heading-secondary">Exiting tours for adventurous people</h2>
+    </div>
+
+    <div class="row">
+      <div class="col-1-of-2">
+        <h3 class="heading-tertiary u-margin-bottom-small">
+          You're going to fall in love with nature
+        </h3>
+        <p class="paragraph">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum
+          consequatur rem repudiandae optio itaque! Cupiditate libero possimus
+          asperiores aperiam, nulla sunt quaerat deleniti sint accusamus
+          laudantium praesentium.
+        </p>
+
+        <h3 class="heading-tertiary u-margin-bottom-small">
+          Live adventures like you nver have before
+        </h3>
+        <p class="paragraph">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam
+          delectus beatae ducimus quo cupiditate quod exercitationem, pariatur
+          accusantium.
+        </p>
+
+        <a href="#" class="btn-text">Learn more &rarr;</a>
+      </div>
+      <div class="col-1-of-2">Image Composition</div>
+    </div>
+  </section>
+
   <!--   
   <section class="grid-test">
     <div class="row">
